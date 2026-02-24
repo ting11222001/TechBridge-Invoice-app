ecbb0491910716d66970ff2683d641b087f9acee
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-02-24 13:35:14 +1030

    Set up basics routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './component/login/login';
+import { Register } from './component/register/register';
+import { Resetpassword } from './component/resetpassword/resetpassword';
+import { Verify } from './component/verify/verify';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login},
+    { path: 'register', component: Register},
+    { path: 'resetpassword', component: Resetpassword},
+    { path: 'user/verify/account/:key', component: Verify},
+    { path: 'user/verify/password/:key', component: Verify},
+    { path: '**', component: Login}, // If enter some non existing routes, go to the Login page
+];
