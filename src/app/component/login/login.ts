abc91364208dca66772aeb3dc5fdc5fd41c0e001
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-02-28 11:35:05 +1030

    Replace BehaviorSubject in the Login component and object destructuring from NgForm object

--- src/app/component/login/login.ts
+++ src/app/component/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from '../../enum/datastate.enum';
 import { LoginState } from '../../interface/appstates';
@@ -17,45 +17,58 @@ import { CommonModule } from '@angular/common';
 export class Login {
   loginState$: Observable<LoginState> = of({ dataState: DataState.LOADED });
   
-  private phoneSubject = new BehaviorSubject<string>('');
-  private emailSubject = new BehaviorSubject<string>('');
+  phone = signal<string>('');
+  email = signal<string>('');
 
   readonly DataState = DataState;
   
   constructor(private router: Router, private userService: User) {}
 
   login(loginForm: NgForm): void {
-    this.loginState$ = this.userService.login$(loginForm.value.email, loginForm.value.password)
+    const { email, password } = loginForm.value;
+    // console.log(loginForm);
+
+    this.loginState$ = this.userService.login$(email, password)
       .pipe(
         map(response => {
           if (!response.data) {
             throw new Error('Response not exists!');
           }
           if (response.data.user?.usingMfa) {
-            this.phoneSubject.next(response.data.user.phone ?? '');
-            this.emailSubject.next(response.data.user.email);
+            this.phone.set(response.data.user.phone ?? '');
+            this.email.set(response.data.user.email);
             return {
-              dataState: DataState.LOADED, isUsingMfa: true, loginSuccess: false,
+              dataState: DataState.LOADED, 
+              isUsingMfa: true, 
+              loginSuccess: false,
               phone: response.data.user.phone ? 
-                response.data.user.phone.substring(response.data.user.phone.length - 4) // only need the last 4 digits
+                response.data.user.phone.substring(response.data.user.phone.length - 4) // only need the last 4 digits; currently using the signal state in html
                 : ''
             };
           } else {
             localStorage.setItem(Key.TOKEN, response.data.access_token);
             localStorage.setItem(Key.REFRESH_TOKEN, response.data.refresh_token);
             this.router.navigate(['/']); // to the home page
-            return { dataState: DataState.LOADED, loginSuccess: true };
+            return { 
+              dataState: DataState.LOADED,
+              loginSuccess: true };
           }
         }),
-        startWith({ dataState: DataState.LOADING, isUsingMfa: false }),
+        startWith({ 
+          dataState: DataState.LOADING, 
+          isUsingMfa: false }),
         catchError((error: string) => 
-            of({ dataState: DataState.ERROR, isUsingMfa: false, loginSuccess: false, error })
+            of({ 
+              dataState: DataState.ERROR, 
+              isUsingMfa: false, 
+              loginSuccess: false, 
+              error })
         )
       )
   }
 
   verifyCode(verifyCodeForm: NgForm): void {
-    this.loginState$ = this.userService.verifyCode$(this.emailSubject.value, verifyCodeForm.value.code)
+    this.loginState$ = this.userService.verifyCode$(this.email(), verifyCodeForm.value.code)
       .pipe(
         map(response => {
             if (!response.data) {
@@ -64,12 +77,21 @@ export class Login {
             localStorage.setItem(Key.TOKEN, response.data.access_token);
             localStorage.setItem(Key.REFRESH_TOKEN, response.data.refresh_token);
             this.router.navigate(['/']); // to the home page
-            return { dataState: DataState.LOADED, loginSuccess: true };
+            return { 
+              dataState: DataState.LOADED, 
+              loginSuccess: true };
           
         }),
-        startWith({ dataState: DataState.LOADING, isUsingMfa: false }),
+        startWith({ 
+          dataState: DataState.LOADING, 
+          isUsingMfa: false }),
         catchError((error: string) => 
-            of({ dataState: DataState.ERROR, isUsingMfa: false, loginSuccess: false, error })
+            of({ 
+              dataState: DataState.ERROR, 
+              isUsingMfa: false, 
+              loginSuccess: false, 
+              error 
+            })
         )
       )
   }
