26414c3176768f312702abff46a4ec61dd9d93e2
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-03-19 23:20:12 +1030

    Renovate the navbar

--- src/app/component/navbar/navbar.ts
+++ src/app/component/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { User } from '../../interface/user';
 import { UserService } from '../../service/user';
@@ -12,10 +12,33 @@ import { UserService } from '../../service/user';
 export class NavbarComponent {
   @Input() user: User | undefined;
 
+  customersOpen = false;
+  invoicesOpen = false;
+  profileOpen = false;
+  mobileOpen = false;
+
   constructor(private router: Router, private userService: UserService) {}
-  
+
   logOut(): void {
     this.userService.logOut$();
     this.router.navigate(['/login']);
+  }
+
+  toggleCustomers(): void {
+    this.customersOpen = !this.customersOpen;
+    this.invoicesOpen = false;
+    this.profileOpen = false;
+  }
+
+  toggleInvoices(): void {
+    this.invoicesOpen = !this.invoicesOpen;
+    this.customersOpen = false;
+    this.profileOpen = false;
+  }
+
+  toggleProfile(): void {
+    this.profileOpen = !this.profileOpen;
+    this.customersOpen = false;
+    this.invoicesOpen = false;
   }
 }
